a93140209783134dcbb9c9c07ad6c8274c4eb93c
SGM24 <beckers.rival@gmail.com>
2026-06-25 01:10:21 -0400

    Basic functionality
    - Requires range fix

---
+++ src/example.py
@@ -0,0 +1,35 @@
+import menucli as menu
+
+class ExitMenuException(Exception):
+    pass
+
+def My_callback(value):
+    print()
+    print("Hello from the callback, this is what i recieved : ", value)
+
+def My_function():
+    print()
+    print("Hello from the function")
+
+def exit_loop():
+    return False
+
+Menu : menu.MenuItem = [
+    menu.MenuItem("String", "Enter a String : ", str, My_callback),
+    menu.MenuItem("Int", "Enter a number : ", int, My_callback),
+     menu.MenuItem("Int 5-10", "Enter a number : ", int, My_callback, 5, 10),
+    menu.MenuItem("Function","", callable, My_function),
+    menu.MenuItem("Exit","",callable,exit_loop)
+]
+
+print("\n Start \n")
+
+menu.goToMenu(Menu)
+
+try:
+    while menu.render():
+        print()
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt!")
+
+print("Exited loop")
---
+++ src/menucli.py
@@ -0,0 +1,54 @@
+import colors
+from collections.abc import Callable
+
+
+class MenuItem:
+    def __init__(self, name : str, value_name : str, type : type, callback : Callable[[any], None] = None, value_min = None, value_max = None):
+        self.name = name
+        self.value_name = value_name
+        self.type = type
+        self.callback = callback
+        self.value_min = value_min
+        self.value_max = value_max
+
+menu_stack : list[list[MenuItem]] = []
+
+def goToMenu(menu : list[MenuItem], append = False):
+    if append:
+        menu_stack.append(menu)
+    else:
+        menu_stack.clear()
+        menu_stack.append(menu)
+
+
+def _ask_value(type : type, text : str,zero_indexed : bool = False, min = None, max = None):
+    while True:
+        while True:
+            try:
+                answer = input(text)
+                result = type(answer)
+                if (min == None and max == None) : return result
+                if (type == int):
+                    result -= int(zero_indexed)
+                    if not (result < min or result > max) : return result
+                    else: colors.colorprint(f"[ERR] Invalid input, must be btween {min + int(zero_indexed)} and {max + int(zero_indexed)}!","red")
+            except ValueError:
+                colors.colorprint(f"[ERR] Invalid input, must be a {type.__name__}!","red")
+
+def render():
+    stack_length = len(menu_stack)
+    if stack_length == 0 : return
+    current_menu : list[MenuItem] = menu_stack[-1]
+    current_menu_length : int = len(current_menu) - 1
+
+    for index, item in enumerate(current_menu):
+        print(index + 1, item.name)
+    print()
+    selection : MenuItem = current_menu[_ask_value(int, "Select an option : ",True,0,current_menu_length)]
+    if (selection.type is not callable):
+        selection_value = _ask_value(selection.type, selection.value_name, selection.value_min, selection.value_max)
+        if (selection.callback): selection.callback(selection_value)
+    else :
+        result = selection.callback()
+        if result is False : return result
+    return True
