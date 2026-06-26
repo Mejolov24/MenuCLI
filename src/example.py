ce39337ece36950c4c4d8868b3ad16a98b998022
SGM24 <beckers.rival@gmail.com>
2026-06-25 23:57:25 -0400

    changed whole menu and item system
    - removed index and menu argument for item callback.
    - added global callback for easier usage.
    - made settings per menu.

--- src/example.py
+++ src/example.py
@@ -1,36 +1,38 @@
 import menucli as menu
 
-class ExitMenuException(Exception):
-    pass
+def My_menu_callback(index, value = None): # will return none if it doesnt hold a value
+    print("Hello from the menu callback, this is what i recieved : ", value, " From index : ", index)
 
-def My_callback(index, value):
-    print()
-    print("Hello from the callback, this is what i recieved : ", value, " From index : ", index)
+def My_custom_callback(value = None): # will return none if it doesnt hold a value
+    print("Hello from the custom callback, this is what i recieved : ", value)
+
 
 def My_function():
-    print()
     print("Hello from the function")
 
-SubMenu : menu.MenuItem = [
-    menu.MenuItem("String", str,"Enter a String : ", My_callback),
-    menu.MenuItem("Exit",type=menu.Exit)
-]
+SubMenu = menu.Menu([
+    menu.MenuItem("String", str,"Enter a String : "),
+    menu.MenuItem("Exit",menu.Exit)
+],
+My_menu_callback, True, "\033[H\033[2J")
 
-Menu : menu.MenuItem = [
-    menu.MenuItem("String", str ,"Enter a String : ", My_callback),
-    menu.MenuItem("Int", int, "Enter a number : " , My_callback),
-    menu.MenuItem("Int 5-10", int, "Enter a number : " , My_callback, 5, 10),
-    menu.MenuItem("Function",type=callable, target=My_function),
-    menu.MenuItem("SubMenu",type=menu.SubMenu,target=SubMenu),
-    menu.MenuItem("Exit",type=menu.Exit)
-]
+Menu = menu.Menu([
+    menu.MenuItem("String", str ,"Enter a String : "),
+    menu.MenuItem("Int", int, "Enter a number : " ),
+    menu.MenuItem("Int 5-10", int, "Enter a number : " , 5, 10),
+    menu.MenuItem("Int custom callback", int, "Enter a number : " , callback=My_custom_callback),
+    menu.MenuItem("Function",callable, target=My_function),
+    menu.MenuItem("SubMenu - zero indexed",menu.Menu,target=SubMenu),
+    menu.MenuItem("Exit",menu.Exit)
+],
+My_menu_callback, False, "\033[H\033[2J")
 
-print("\n Start \n")
+print("\nStart\n")
 
 menu.goToMenu(Menu)
 
 try:
-    while menu.render(False,"\033[H\033[2J"):
+    while menu.render():
         print()
 except KeyboardInterrupt:
     print("\nKeyboard interrupt!")
--- src/menucli.py
+++ src/menucli.py
@@ -2,20 +2,26 @@ from collections.abc import Callable
 
 
 class MenuItem:
-    def __init__(self, name : str, type : type, value_name : str = None , target : Callable[[any], None] = None, value_min = None, value_max = None):
+    def __init__(self, name : str, type : type, value_name : str = None , value_min = None, value_max = None,  target = None, callback : Callable[[int], None] = None):
         self.name = name
         self.value_name = value_name
         self.type = type
+        self.callback = callback
         self.target = target
         self.value_min = value_min
         self.value_max = value_max
 
-class SubMenu: pass
+class Menu:
+    def __init__(self,items : list[MenuItem], callback : Callable[[int,int], None] = None, zero_indexed : bool = False, separator : str = ""):
+        self.items = items
+        self.callback = callback
+        self.zero_indexed = zero_indexed
+        self.separator = separator
 class Exit: pass
 
-menu_stack : list[list[MenuItem]] = []
+menu_stack : list[Menu] = []
 
-def goToMenu(menu : list[MenuItem], append = False):
+def goToMenu(menu : Menu, append = False):
     if append:
         menu_stack.append(menu)
     else:
@@ -34,37 +40,47 @@ def _ask_value(type : type, text : str, min = None, max = None):
                 if (min == None and max == None) : return result
                 if (type == int):
                     if not (result < min or result > max) : return result
-                    else: print("\033[31m", "[ERR] Invalid input, must be btween {min} and {max}!","\033[0m")
+                    else: print("\033[31m", f"[ERR] Invalid input, must be btween {min} and {max}!","\033[0m")
             except ValueError:
                 print("\033[31m", f"[ERR] Invalid input, must be a {type.__name__}!","\033[0m")
 
-def render(zero_indexed : bool = False, separator : str = ""):
+def render():
     if len(menu_stack) == 0 : return False
-    current_menu : list[MenuItem] = menu_stack[-1]
-    current_menu_length : int = len(current_menu)
-    print(separator)
-    for index, item in enumerate(current_menu):
-        print(index + int(not zero_indexed), item.name)
-    selection_index : int = _ask_value(int, " Select an option : ",0 + int(not zero_indexed),current_menu_length) - int(not zero_indexed)
-    selection : MenuItem = current_menu[selection_index]
+    current_menu : Menu = menu_stack[-1]
+    current_menu_items : list[MenuItem] = current_menu.items
+    current_menu_length : int = len(current_menu_items) - int(current_menu.zero_indexed)
+    print(current_menu.separator)
+    for index, item in enumerate(current_menu_items):
+        print(index + int(not current_menu.zero_indexed), item.name)
+    selection_index : int = _ask_value(int, " Select an option : ",0 + int(not current_menu.zero_indexed),current_menu_length) - int(not current_menu.zero_indexed)
+    selection : MenuItem = current_menu_items[selection_index]
+
+    def callback(value):
+        if (current_menu.callback) : current_menu.callback(selection_index,value)
+        if (selection.callback): selection.callback(value)
+
     match (selection.type):
-        case x if x is callable:
-            result = selection.target()
-            if result is False : return result
+        case x if x == callable:
+            selection.target()
+            callback(None)
         
-        case x if x is SubMenu:
-            print(separator)
+        case x if x == Menu:
+            print(current_menu.separator)
             goToMenu(selection.target,True)
+            callback(None)
             return render()
 
-        case x if x is Exit:
+        case x if x == Exit:
             if (len(menu_stack) > 0):
-                print(separator)
+                print(current_menu.separator)
                 goBack()
+                callback(None)
                 return render()
-            else : return False
+            else :
+                callback(None)
+                return False
 
         case _:
             selection_value = _ask_value(selection.type, "  " + selection.value_name, selection.value_min, selection.value_max)
-            if (selection.target): selection.target(selection_index,selection_value)
+            callback(selection_value)
     return True
